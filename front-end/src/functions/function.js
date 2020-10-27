6abbb4091f9aee29bb04b9e79d2cb25085090ff5
fuseSavage <chaiwat.si.61@ubu.ac.th>
2020-10-27 17:21:33 +0700

    add

--- front-end/src/functions/function.js
+++ front-end/src/functions/function.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const apiHost = "https://com-math.herokuapp.com/";
+const apiHost = "https://com-math.herokuapp.com";
 
 export const getB2s = (b2s) => {
     return axios
